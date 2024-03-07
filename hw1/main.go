99ef195631adceaf6c4c30e55d9344db924312c1
Vsevolod <tankict.92@gmail.com>
2024-03-07 18:08:51 +0200

    feature hw1

---
+++ hw1/main.go
@@ -0,0 +1,17 @@
+package main
+
+import "fmt"
+
+func main() {
+	fullName := "Наполеон I Бонапарт"
+	fmt.Printf("Повне ім'я: %s\n", fullName)
+
+	activity := "Військовий Діяч, Полководець"
+	fmt.Printf("Діяльність: %s\n", activity)
+
+	birthday := "15 Серпня 1769"
+	fmt.Printf("Дата народження: %s\n", birthday)
+
+	yearsOld := 51
+	fmt.Printf("Він прожив: %d рік", yearsOld)
+}
